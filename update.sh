616ac08572a1ce08b728e329a3332d7983c4ac44
Tianon Gravi <admwiggin@gmail.com>
2015-04-29 13:55:38 -0600

    Add "last modified" information for each version of GCC

--- update.sh
+++ update.sh
@@ -9,16 +9,21 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
-packagesUrl="http://ftpmirror.gnu.org/gcc/"
+#packagesUrl='http://ftpmirror.gnu.org/gcc/'
+packagesUrl='http://mirrors.ibiblio.org/gnu/ftp/gnu/gcc/' # the actual HTML of the page changes based on which mirror we end up hitting, so let's hit a specific one for now... :'(
 packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
 curl -sSL "$packagesUrl" > "$packages"
 
 for version in "${versions[@]}"; do
 	fullVersion="$(grep '<a href="gcc-'"$version." "$packages" | sed -r 's!.*<a href="gcc-([^"/]+)/?".*!\1!' | sort -V | tail -1)"
+	lastModified="$(grep -m1 '<a href="gcc-'"$fullVersion"'/"' "$packages" | sed -r 's!.*<td class="m">([^<>]+)</td>.*!\1!')"
 	
 	(
 		set -x
-		sed -ri 's/^(ENV GCC_VERSION) .*/\1 '"$fullVersion"'/;' "$version/Dockerfile"
+		sed -ri '
+			s/^(ENV GCC_VERSION) .*/\1 '"$fullVersion"'/;
+			s/^(# Last Modified:) .*/\1 '"$lastModified"'/;
+		' "$version/Dockerfile"
 	)
 done
 
