0e9a45e0f809742ca8c85937ecb3b554700182f4
Tianon Gravi <admwiggin@gmail.com>
2015-04-29 15:46:04 -0600

    Add EOL warning to update.sh (and switch to a different mirror with a date format that "date" is happy to parse)

--- update.sh
+++ update.sh
@@ -10,13 +10,29 @@ fi
 versions=( "${versions[@]%/}" )
 
 #packagesUrl='http://ftpmirror.gnu.org/gcc/'
-packagesUrl='http://mirrors.ibiblio.org/gnu/ftp/gnu/gcc/' # the actual HTML of the page changes based on which mirror we end up hitting, so let's hit a specific one for now... :'(
+packagesUrl='https://mirrors.kernel.org/gnu/gcc/' # the actual HTML of the page changes based on which mirror we end up hitting, so let's hit a specific one for now... :'(
 packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
 curl -sSL "$packagesUrl" > "$packages"
 
+# our own "supported" window is 2 years because upstream doesn't have a good guideline, but appears to only release maintenance updates for 2-3 years
+export TZ=UTC
+eolPeriod='2 years'
+today="$(date +'%s')"
+eolAgo="$(date +'%s' -d "$(date -d "@$today") - $eolPeriod")"
+eolAge="$(( $today - $eolAgo ))"
+eols=()
+
+dateFormat='%Y-%m-%d'
+
 for version in "${versions[@]}"; do
 	fullVersion="$(grep '<a href="gcc-'"$version." "$packages" | sed -r 's!.*<a href="gcc-([^"/]+)/?".*!\1!' | sort -V | tail -1)"
-	lastModified="$(grep -m1 '<a href="gcc-'"$fullVersion"'/"' "$packages" | sed -r 's!.*<td class="m">([^<>]+)</td>.*!\1!')"
+	lastModified="$(grep -m1 '<a href="gcc-'"$fullVersion"'/"' "$packages" | awk -F '  +' '{ print $2 }')"
+	lastModified="$(date -d "$lastModified" +"$dateFormat")"
+	
+	releaseAge="$(( $today - $(date +'%s' -d "$lastModified") ))"
+	if [ $releaseAge -gt $eolAge ]; then
+		eols+=( "$version ($fullVersion)" )
+	fi
 	
 	(
 		set -x
@@ -26,5 +42,21 @@ for version in "${versions[@]}"; do
 		' "$version/Dockerfile"
 	)
 done
+
+if [ ${#eols[@]} -gt 0 ]; then
+	{
+		echo
+		echo
+		echo
+		echo " WARNING: the following releases are older than $eolPeriod:"
+		echo
+		for eol in "${eols[@]}"; do
+			echo "   - $eol"
+		done
+		echo
+		echo
+		echo
+	} >&2
+fi
 
 rm "$packages"
