3b33871fe9558262cb5ed6253d358f76710e9ccb
Tianon Gravi <admwiggin@gmail.com>
2017-06-16 10:45:38 -0700

    Add more (future) helpful bits, TODO/comments, and exclude i386 for now

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -69,7 +69,8 @@ for version in "${versions[@]}"; do
 	commit="$(dirCommit "$version")"
 	parent="$(awk 'toupper($1) == "FROM" { print $2 }' "$version/Dockerfile")"
 	# no arm32 for now: https://github.com/docker-library/gcc/issues/37
-	arches="$(echo " ${parentRepoToArches[$parent]} " | sed -r 's/ arm32v[^ ]+ / /g')"
+	# no i386 for now: https://github.com/docker-library/gcc/issues/38
+	arches="$(echo " ${parentRepoToArches[$parent]} " | sed -r -e 's/ (arm32v[^ ]+|i386) / /g')"
 
 	dockerfile="$(git show "$commit":"$version/Dockerfile")"
 	fullVersion="$(echo "$dockerfile" | awk '$1 == "ENV" && $2 == "GCC_VERSION" { print $3; exit }')"
